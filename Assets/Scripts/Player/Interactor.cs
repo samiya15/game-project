fb860886da4245c08191e5b2951a9039d7a61cd7
Terry <167083271+167146@users.noreply.github.com>
2026-07-05 20:09:00 +0300

    Camera position

---
+++ Assets/Scripts/Player/Interactor.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    public float interactRange = 3f;
+
+    public LayerMask interactLayer;
+
+    private IInteractable currentTarget;
+
+    void Update()
+    {
+        FindTarget();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentTarget != null)
+            {
+                currentTarget.Interact();
+            }
+        }
+    }
+
+    void FindTarget()
+    {
+        currentTarget = null;
+
+        Collider[] hits =
+            Physics.OverlapSphere(
+                transform.position,
+                interactRange,
+                interactLayer
+            );
+
+        float closest = Mathf.Infinity;
+
+        foreach (Collider hit in hits)
+        {
+            IInteractable interactable =
+                hit.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                float distance =
+                    Vector3.Distance(
+                        transform.position,
+                        hit.transform.position
+                    );
+
+                if (distance < closest)
+                {
+                    closest = distance;
+                    currentTarget = interactable;
+                }
+            }
+        }
+
+        if (
+            InteractionPromptUI.Instance != null
+        )
+        {
+            InteractionPromptUI.Instance.ShowTarget(currentTarget);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(
+            transform.position,
+            interactRange
+        );
+    }
+}
